95edfa11d05740bdcc075168b3de49830443ffe0
mehedihasanshakil7 <mehedihshakil7@gmail.com>
2023-08-07 15:13:08 +0600

    calculator ui created

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,125 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    ),
-  );
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _input = '';
+  double _result = 0;
+
+  void _onButtonPressed(String value) {
+    setState(() {
+      if (value == '=') {
+        try {
+          _result = evalExpression(_input);
+          _input = _result.toString();
+        } catch (e) {
+          _input = 'Error';
+        }
+      } else {
+        _input += value;
+      }
+    });
+  }
+
+  double evalExpression(String expression) {
+    // Implement your expression evaluation logic here
+    // For simplicity, this example uses eval from 'package:expression_language'
+    return double.parse(expression);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black12,
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                _input,
+                style: TextStyle(fontSize: 36.0),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('+'),
+              _buildButton('-'),
+              _buildButton('*'),
+              _buildButton('/'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('7'),
+              _buildButton('8'),
+              _buildButton('9'),
+              _buildButton('='),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('4'),
+              _buildButton('5'),
+              _buildButton('6'),
+              _buildButton('C'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('1'),
+              _buildButton('2'),
+              _buildButton('3'),
+              _buildButton('0'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String label) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: () => _onButtonPressed(label),
+          child: Text(
+            label,
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
 }
